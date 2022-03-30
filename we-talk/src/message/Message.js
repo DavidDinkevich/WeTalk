a4e77de1bcb43c36d13323fea526b7f2329323ff
David D <david.dinkevich@gmail.com>
2022-03-30 21:07:42 +0300

    Created component for individual messages

---
+++ we-talk/src/message/Message.js
@@ -0,0 +1,55 @@
+
+function Message({ source, author, message }) {
+    const messageSide = source === 'self' ? 'msg-self' : 'msg-remote';
+    if (source === 'remote') {
+        return (
+            <article className="msg-container msg-remote" id="msg-0">
+                <div className="msg-box">
+                    <img
+                        className="user-img"
+                        id="user-0"
+                        src="//gravatar.com/avatar/00034587632094500000000000000000?d=retro"
+                        alt='???'
+                    />
+                    <div className="flr">
+                        <div className="messages">
+                            <p className="msg" id="msg-0">
+                                {message}
+                            </p>
+                        </div>
+                        <span className="timestamp" style={{ height: '100%' }}>
+                            <span className="username">Name</span>•
+                            <span className="posttime">3 minutes ago</span>
+                        </span>
+                    </div>
+                </div>
+            </article>
+        );
+    } else {
+        return (
+            <article className="msg-container msg-self" id="msg-0">
+                <div className="msg-box">
+                    <div className="flr">
+                        <div className="messages">
+                            <p className="msg" id="msg-1">
+                                {message}
+                            </p>
+                        </div>
+                        <span className="timestamp">
+                            <span className="username">Name</span>•
+                            <span className="posttime">Now</span>
+                        </span>
+                    </div>
+                    <img
+                        className="user-img"
+                        id="user-0"
+                        src="//gravatar.com/avatar/56234674574535734573000000000001?d=retro"
+                        alt='???'
+                    />
+                </div>
+            </article>
+        );
+    }
+}
+
+export default Message;
