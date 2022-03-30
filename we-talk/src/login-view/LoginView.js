e8f2077ef887d094853bfcf1c9abef6313fa3559
ShacharMoreshet <shachar.moreshet@live.biu.ac.il>
2022-03-30 13:55:07 +0300

    Cleaned up code in login view

--- we-talk/src/login-view/LoginView.js
+++ we-talk/src/login-view/LoginView.js
@@ -22,38 +22,7 @@ function LoginView() {
 
                                 }></Route>
                                 <Route path='/sign-up' element={
-                                    <div className="col" >
-                                        <div style={{ background: 'white', borderRadius: '20px', paddingBottom: '20px', paddingTop: '20px' }}>
-                                            <form className="px-4 py-3" style={{ float: 'center' }}>
-                                                <div className="mb-3">
-                                                    {/* <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label> */}
-                                                    <input type="email" className="form-control" id="emailField" placeholder="email@example.com"
-                                                        style={{ lineHeight: '3' }}></input>
-                                                </div>
-                                                <div className="mb-3">
-                                                    {/* <label htmlFor="exampleDropdownFormPassword1" className="form-label">Password</label> */}
-                                                    <input type="password" className="form-control" id="passwordField" placeholder="Password"
-                                                        style={{ lineHeight: '3' }}></input>
-                                                </div>
-                                                <div className="mb-3">
-                                                    <input type="text" className="form-control" id="displayNameField" placeholder="Display name"
-                                                        style={{ lineHeight: '3' }}></input>
-                                                </div>
-                                                <div className="mb-3">
-                                                    <div className="form-check">
-                                                        <input type="checkbox" className="form-check-input" id="dropdownCheck"></input>
-                                                        <label className="form-check-label" htmlFor="dropdownCheck">
-                                                            Remember me
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <button type="submit" className="btn btn-primary">Sign up</button>
-                                            </form>
-                                            <div className="dropdown-divider"></div>
-                                            <a className="dropdown-item" href="/">Already have an account? Sign in</a>
-                                            <a className="dropdown-item" href="/">Forgot password?</a>
-                                        </div>
-                                    </div>
+                                    <SignupDialog />
                                 }></Route>
                             </Routes>
                         </BrowserRouter>
@@ -69,35 +38,72 @@ function LoginView() {
 function LoginDialog() {
     return (
         <div className="col" >
-        <div style={{ background: 'white', borderRadius: '20px', paddingBottom: '20px', paddingTop: '20px' }}>
-            <form className="px-4 py-3" style={{ float: 'center' }}>
-                <div className="mb-3">
-                    {/* <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label> */}
-                    <input type="email" className="form-control" id="emailField" placeholder="email@example.com"
-                        style={{ lineHeight: '3' }}></input>
-                </div>
-                <div className="mb-3">
-                    {/* <label htmlFor="exampleDropdownFormPassword1" className="form-label">Password</label> */}
-                    <input type="password" className="form-control" id="passwordField" placeholder="Password"
-                        style={{ lineHeight: '3' }}></input>
-                </div>
-                <div className="mb-3">
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="dropdownCheck"></input>
-                        <label className="form-check-label" htmlFor="dropdownCheck">
-                            Remember me
-                        </label>
+            <div style={{ background: 'white', borderRadius: '20px', paddingBottom: '20px', paddingTop: '20px' }}>
+                <form className="px-4 py-3" style={{ float: 'center' }}>
+                    <div className="mb-3">
+                        {/* <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label> */}
+                        <input type="email" className="form-control" id="emailField" placeholder="email@example.com"
+                            style={{ lineHeight: '3' }}></input>
                     </div>
-                </div>
-                <button type="submit" className="btn btn-primary">Sign in</button>
-            </form>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="/sign-up">New around here? Sign up</a>
-            <a className="dropdown-item" href="/">Forgot password?</a>
+                    <div className="mb-3">
+                        {/* <label htmlFor="exampleDropdownFormPassword1" className="form-label">Password</label> */}
+                        <input type="password" className="form-control" id="passwordField" placeholder="Password"
+                            style={{ lineHeight: '3' }}></input>
+                    </div>
+                    <div className="mb-3">
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="dropdownCheck"></input>
+                            <label className="form-check-label" htmlFor="dropdownCheck">
+                                Remember me
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Sign in</button>
+                </form>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="/sign-up">New around here? Sign up</a>
+                <a className="dropdown-item" href="/">Forgot password?</a>
 
+            </div>
         </div>
-    </div>
 
+    );
+}
+
+function SignupDialog() {
+    return (
+        <div className="col" >
+            <div style={{ background: 'white', borderRadius: '20px', paddingBottom: '20px', paddingTop: '20px' }}>
+                <form className="px-4 py-3" style={{ float: 'center' }}>
+                    <div className="mb-3">
+                        {/* <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label> */}
+                        <input type="email" className="form-control" id="emailField" placeholder="email@example.com"
+                            style={{ lineHeight: '3' }}></input>
+                    </div>
+                    <div className="mb-3">
+                        {/* <label htmlFor="exampleDropdownFormPassword1" className="form-label">Password</label> */}
+                        <input type="password" className="form-control" id="passwordField" placeholder="Password"
+                            style={{ lineHeight: '3' }}></input>
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" id="displayNameField" placeholder="Display name"
+                            style={{ lineHeight: '3' }}></input>
+                    </div>
+                    <div className="mb-3">
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="dropdownCheck"></input>
+                            <label className="form-check-label" htmlFor="dropdownCheck">
+                                Remember me
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Sign up</button>
+                </form>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="/">Already have an account? Sign in</a>
+                <a className="dropdown-item" href="/">Forgot password?</a>
+            </div>
+        </div>
     );
 }
 
