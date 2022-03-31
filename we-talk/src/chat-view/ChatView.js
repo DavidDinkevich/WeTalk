141f4220224440a4ab265e37478320bd2f4d4f61
David D <david.dinkevich@gmail.com>
2022-03-31 16:17:38 +0300

    Fixed positioning/size of attachments button

--- we-talk/src/chat-view/ChatView.js
+++ we-talk/src/chat-view/ChatView.js
@@ -40,18 +40,6 @@ function ChatView() {
                 <section className="chat-window" style={{ position: 'relative', height: '100%' }}>
                     {uilist}
                 </section>
-                <>
-                </>
-
-                <button className='button' style={{ border: 'none', background: 'white' }} >
-                    <span>
-                        <div style={{ position: 'absolute', width: '3%', height: '10%', background: '#7C79D5', top: '90%', left: '0%' }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="90%" height="88%" fill="currentColor" class="bi bi-paperclip" viewBox="0 0 16 16">
-                                <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z" />
-                            </svg>
-                        </div>
-                    </span>
-                </button>
 
                 <div id='footer' className="chat-input" style={{ background: '#7C79D5' }} onKeyDown={(e) => {
                     if (!e) e = window.event;
@@ -59,8 +47,18 @@ function ChatView() {
                     if (keyCode === 'Enter')
                         sendMessage();
                 }}>
+                    <button className='button' style={{ float: 'left', border: 'none', background: '#7C79D5' }} >
+                        {/* <span> */}
+                            <div style={{ background: '#7C79D5' }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" style={{ marginTop: '-4px', width: '60px', height: '60px' }} fill="currentColor" class="bi bi-paperclip" viewBox="0 0 24 24">
+                                    <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z" />
+                                </svg>
+                            </div>
+                        {/* </span> */}
+                    </button>
 
                     <input type="text" id='message_box' autoComplete="on" placeholder="Type a message" />
+
                     <button onClick={sendMessage}>
                         <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
                             <path
