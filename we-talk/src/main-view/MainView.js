7964fa43ded9d757aba4fe387b4e53ed11de504c
David D <david.dinkevich@gmail.com>
2022-04-01 12:49:21 +0300

    Got rid of horizontal sidebar in chat-view and fixed bug where page was overflowing

--- we-talk/src/main-view/MainView.js
+++ we-talk/src/main-view/MainView.js
@@ -4,11 +4,11 @@ import ChatView from '../chat-view/ChatView.js'
 
 function MainView({ chatList, selfInfo }) {
     return (
-        <div className='row g-0' style={{ height: '90vh', width: '90vw', minWidth: '300px', background:'#F4F3FF'}}>
+        <div className='row g-0' style={{ maxHeight: '90vh', height: '90vh', width: '90vw', minWidth: '300px', background:'#F4F3FF'}}>
             <div className='col-lg-4' style={{ background: 'rgb(240, 240, 240)', width: '26%', padding:'0px' }}>
                 <LeftScreen chatList={chatList} selfInfo={selfInfo} />
             </div>
-            <div className='col'>
+            <div className='col' style={{maxHeight:'inherit'}}>
                 <ChatView />
             </div>
 
