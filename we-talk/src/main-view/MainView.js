2d6fe9d3b6853f8fa67572c4e9c8f675ab4da708
shacharMoreshet <92321514+shacharMoreshet@users.noreply.github.com>
2022-04-05 15:40:35 +0300

    added an opening screen when entering the chat screen, and when we click on contact the chat-view opens

--- we-talk/src/main-view/MainView.js
+++ we-talk/src/main-view/MainView.js
@@ -1,15 +1,26 @@
 
 import LeftScreen from '../left-screen/LeftScreen.js';
-import ChatView from '../chat-view/ChatView.js'
+import ChatView from '../chat-view/ChatView.js';
+import React, { useState } from "react";
+import Logo from '../logo/Logo.js';
 
-function MainView({ selfInfo, activeContact, setActiveContact}) {
+export let showChatView;
+
+function MainView({ selfInfo, activeContact, setActiveContact }) {
+
+    const [chatViewHidden, setChatViewHidden] = useState(true);
+    showChatView = () => setChatViewHidden(false);
+
     return (
-        <div className='row g-0' style={{ maxHeight: '90vh', height: '90vh', width: '90vw', minWidth: '300px', background:'#F4F3FF'}}>
-            <div className='col-lg-4' style={{ background: 'rgb(240, 240, 240)', width: '26%', padding:'0px' }}>
-                <LeftScreen selfInfo={selfInfo} activeContact={activeContact} setActiveContact={setActiveContact}/>
+        <div className='row g-0' style={{ maxHeight: '90vh', height: '90vh', width: '90vw', minWidth: '300px', background: '#F4F3FF' }}>
+            <div className='col-lg-4' style={{ background: 'rgb(240, 240, 240)', width: '26%', padding: '0px' }}>
+                <LeftScreen selfInfo={selfInfo} activeContact={activeContact} setActiveContact={setActiveContact} />
             </div>
-            <div className='col' style={{maxHeight:'inherit'}}>
-                <ChatView activeContact={activeContact}/>
+
+
+
+            <div className='col' style={{ maxHeight: 'inherit' , textAlign: 'center' }}>
+                {!chatViewHidden ? <ChatView activeContact={activeContact} /> : <Logo />}
             </div>
 
         </div>
