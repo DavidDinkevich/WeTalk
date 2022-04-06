0d03621e950ef3557b694b7fca6513b6b73832d3
shacharMoreshet <92321514+shacharMoreshet@users.noreply.github.com>
2022-04-06 19:44:17 +0300

    Update LoginView.js

--- we-talk/src/login-view/LoginView.js
+++ we-talk/src/login-view/LoginView.js
@@ -127,7 +127,7 @@ export function LoginView() {
 
                             <div className="dropdown-divider"></div>
                             <a className="dropdown-item" href="/sign-up">New around here? Sign up</a>
-                            <a className="dropdown-item" href="/">Forgot password?</a>
+                            {/*<a className="dropdown-item" href="/">Forgot password?</a>*/}
 
                         </div>
                     </div>
@@ -197,7 +197,7 @@ export function SignupView() {
 
                             <div className="dropdown-divider"></div>
                             <a className="dropdown-item" href="/">Already have an account? Sign in</a>
-                            <a className="dropdown-item" href="/">Forgot password?</a>
+                            {/*<a className="dropdown-item" href="/">Forgot password?</a>*/}
                         </div>
                     </div>
 
