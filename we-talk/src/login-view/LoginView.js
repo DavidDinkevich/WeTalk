1f9b95fa1393dedfe9e1f3143489e10c26670067
אביה אלגרבלי <92366161+aviya80@users.noreply.github.com>
2022-04-06 11:40:48 +0300

    fix validation of sign in and sign up

--- we-talk/src/login-view/LoginView.js
+++ we-talk/src/login-view/LoginView.js
@@ -1,12 +1,28 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
+
+
+const check = function () {
+    if (document.getElementById('passwordField').value ==
+        document.getElementById('confirmPassword').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Passwords match!';
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = "Passwords don't match";
+    }
+}
+
 export function LoginView() {
     const onSubmit = function () {
         const userNameField = document.getElementById('userNameField');
         const passwordField = document.getElementById('passwordField');
         console.log(userNameField.value);
         console.log(passwordField.value);
+        window.location.replace("/chat");
+
     }
+
 
 
     return (
@@ -31,10 +47,10 @@ export function LoginView() {
 
                         <div className="col dflex" >
                             <div style={{ background: 'white', borderRadius: '20px', paddingBottom: '20px', paddingTop: '20px' }}>
-                                <form novalidate className="px-4 py-3" style={{ float: 'center' }}>
+                                <form action="" onSubmit={() => { onSubmit(); return false; }} className="px-4 py-3" style={{ float: 'center' }}>
                                     <div className="mb-3">
                                         <div className="input-group has-validation">
-                                            <input type="text" className="form-control" placeholder="Username" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required style={{ lineHeight: '3' }} pattern="^([a-zA-Z@*#]{1,8})$" title="name must be only with characters." />
+                                            <input type="text" className="form-control" placeholder="Username" id="userNameField" aria-describedby="inputGroupPrepend" style={{ lineHeight: '3' }} required pattern="^([a-zA-Z@*#]{1,8})$" title="name must be only with characters." />
                                             <div className="invalid-feedback">
                                                 Please choose a username.
                                             </div>
@@ -43,7 +59,7 @@ export function LoginView() {
 
                                     <div className="mb-3">
                                         <div className="input-group has-validation">
-                                            <input type="password" className="form-control" placeholder="Password" id="passwordField" aria-describedby="inputGroupPrepend" required style={{ lineHeight: '3' }} pattern="^([a-zA-Z0-9@*#]{1,8})$" title="password must be alphanumeric, and no more than 8 charcters." />
+                                            <input type="password" className="form-control" placeholder="Password" id="passwordField" aria-describedby="inputGroupPrepend" style={{ lineHeight: '3' }} required pattern="^([a-zA-Z0-9@*#]{8,100})$" title="password must be alphanumeric, minimum 8 charcters." />
                                             <div className="invalid-feedback">
                                                 Please choose a username.
                                             </div>
@@ -59,7 +75,7 @@ export function LoginView() {
                                         </div>
                                     </div>
 
-                                    <button type="submit" className="btn btn-primary" style={{background:'#0D168F', border:'white', marginLeft: '0%', marginBottom: '1%', marginTop: '0%' }} onClick={onSubmit}>Sign in</button>
+                                    <button type="submit" className="btn btn-primary" style={{ background: '#0D168F', border: 'white', marginLeft: '0%', marginBottom: '1%', marginTop: '0%' }}>Sign in</button>
                                 </form>
 
                                 <div className="dropdown-divider"></div>
@@ -101,18 +117,18 @@ export function SignupView() {
                             <div style={{ background: 'white', borderRadius: '20px', paddingBottom: '20px', paddingTop: '20px' }}>
                                 <form className="px-4 py-3" style={{ float: 'center' }}>
                                     <div className="mb-3">
-                                        {/* <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label> */}
-                                        <input type="email" className="form-control" id="emailField" placeholder="Email"
+                                        <input type="text" className="form-control" id="NameField" aria-describedby="inputGroupPrepend" required pattern="^([a-zA-Z@*#]{1,8})$" title="name must be only with characters." placeholder="Username"
                                             style={{ lineHeight: '3' }}></input>
                                     </div>
                                     <div className="mb-3">
-                                        {/* <label htmlFor="exampleDropdownFormPassword1" className="form-label">Password</label> */}
-                                        <input type="password" className="form-control" id="passwordField" placeholder="Password"
+                                        <input type="password" className="form-control" id="passwordField" required pattern="^([a-zA-Z0-9@*#]{8,100})$" title="password must be alphanumeric, minimum 8 charcters." placeholder="Password"
                                             style={{ lineHeight: '3' }}></input>
+                                        
                                     </div>
                                     <div className="mb-3">
-                                        <input type="text" className="form-control" id="displayNameField" placeholder="Display name"
+                                        <input type="password" onKeyUp={check} className="form-control" id="confirmPassword" placeholder="Confirm Password"
                                             style={{ lineHeight: '3' }}></input>
+                                            <span id='message'></span>
                                     </div>
                                     <div className="mb-3">
                                         <div className="form-check">
@@ -123,7 +139,9 @@ export function SignupView() {
                                         </div>
                                     </div>
                                     <button type="submit" className="btn btn-primary">Sign up</button>
+
                                 </form>
+
                                 <div className="dropdown-divider"></div>
                                 <a className="dropdown-item" href="/">Already have an account? Sign in</a>
                                 <a className="dropdown-item" href="/">Forgot password?</a>
