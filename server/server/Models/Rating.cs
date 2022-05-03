358891d8a68fac0f4154545ab048a1a61a95a140
David D <david.dinkevich@gmail.com>
2022-05-03 17:12:32 +0300

    Created ASP .NET project for part 2

---
+++ server/server/Models/Rating.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models {
+    public class Rating {
+        public int Id { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage = "Value must be between 1 and 5")]
+        [Display(Name = "Rating")]
+        public int RatingsCount { get; set; }
+        [Required]
+        [Display(Name = "Author")]
+        public string Name { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public string Time { get; set; }
+
+    }
+}
